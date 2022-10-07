eaf4ee3f572ca5951e6c8cf936ef1e82aa7dd253
Tanisha <tanishagupta0820@gmail.com>
2022-10-07 22:49:56 +0530

    changes

--- src/Sidenav.js
+++ src/Sidenav.js
@@ -3,15 +3,15 @@ import './Sidenav.css'
 import EventsLogo from './assets/image/flags-svgrepo-com.svg'
 
 function Sidenav() {
-    const [sideNav, setSideNav] = useState(true)
+    const [inactive, setInative] = useState(true)
 
-    const showSidenav = () => setSideNav(!sideNav)
+    const toggleSideNav = () => setInative(!inactive)
 
     return (
         <>
-            <div className={sideNav ? 'nav-container' : 'nav-container active'}>
+            <div className={inactive ? 'nav-container' : 'nav-container active'}>
                 <div className='burger-icon-container'>
-                    <span className='burger' onClick={showSidenav}><i class="bi bi-list"></i></span>
+                    <span className='burger' onClick={toggleSideNav}><i class="bi bi-list"></i></span>
                 </div>
 
                 <div className='main-menu-container'>
@@ -22,7 +22,7 @@ function Sidenav() {
                                     <div className='menu-icon imported-i'>
                                         <i class="bi bi-house"></i>
                                     </div>
-                                    <span className='menu-name'>Home</span>
+                                    <span className={!inactive ? 'menu-name' : 'hidden'}>Home</span>
                                 </a>
                             </li>
                             <li>
@@ -30,14 +30,14 @@ function Sidenav() {
                                     <div className='menu-icon imported-i'>
                                         <i class="bi bi-info-square"></i>
                                     </div>
-                                    <span className='menu-name'>About</span>
+                                    <span className={!inactive ? 'menu-name' : 'hidden'}>About</span>
                                 </a>
                             </li>
                             <li>
                                 <a href='#'><div className='menu-icon'>
                                     <img height={14.5} src={EventsLogo} />
                                 </div>
-                                    <span className='menu-name'>Events</span>
+                                    <span className={!inactive ? 'menu-name' : 'hidden'}>Events</span>
                                 </a>
                             </li>
                             <li>
@@ -45,7 +45,7 @@ function Sidenav() {
                                     <div className='menu-icon imported-i'>
                                         <i class="bi bi-info-square"></i>
                                     </div>
-                                    <span className='menu-name'>Sponsors</span>
+                                    <span className={!inactive ? 'menu-name' : 'hidden'}>Sponsors</span>
                                 </a>
                             </li>
                         </ul>
