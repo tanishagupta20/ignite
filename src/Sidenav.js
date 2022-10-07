b0eeecc822322ea9c162f88f2af8eff7a2340920
Tanisha <tanishagupta0820@gmail.com>
2022-10-07 22:10:02 +0530

    new changes

--- src/Sidenav.js
+++ src/Sidenav.js
@@ -1,8 +1,9 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import './Sidenav.css'
+import EventsLogo from './assets/image/flags-svgrepo-com.svg'
 
 function Sidenav() {
-    const[sideNav, setSideNav] = useState(true)
+    const [sideNav, setSideNav] = useState(true)
 
     const showSidenav = () => setSideNav(!sideNav)
 
@@ -14,6 +15,41 @@ function Sidenav() {
                 </div>
 
                 <div className='main-menu-container'>
+                    <div className='menu-items'>
+                        <ul className='menu-list'>
+                            <li>
+                                <a href='#'>
+                                    <div className='menu-icon'>
+                                        <i class="bi bi-house"></i>
+                                    </div>
+                                    <span>Home</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href='#'>
+                                    <div className='menu-icon'>
+                                        <i class="bi bi-info-square"></i>
+                                    </div>
+                                    <span>About</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href='#'><div className='menu-icon'>
+                                    <img height={15} src={EventsLogo} />
+                                </div>
+                                    <span>Events</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href='#'>
+                                    <div className='menu-icon'>
+                                        <i class="bi bi-info-square"></i>
+                                    </div>
+                                    <span>Sponsors</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
 
                 </div>
             </div>
