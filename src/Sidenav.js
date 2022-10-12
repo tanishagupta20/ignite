c8b786c599617b9c5033018b68cb0a8b7ae3e519
Tanisha <tanishagupta0820@gmail.com>
2022-10-12 14:21:36 +0530

    updated files

--- src/Sidenav.js
+++ src/Sidenav.js
@@ -10,6 +10,7 @@ function Sidenav() {
 
     return (
         <div className='sidenav-container'>
+            <div></div>
             <div className={inactive ? 'nav-container' : 'nav-container active'}>
                 <div className='burger-icon-container'>
                     <span className='burger' onClick={toggleSideNav}><i class="bi bi-list"></i></span>
@@ -18,46 +19,46 @@ function Sidenav() {
                 <div className='main-menu-container' onClick={toggleSideNav}>
                     <div className='menu-items'>
                         <ul className='menu-list'>
-                            <li style={!inactive ? {width : "210px"} : {width : "20px"}} className = "home-icon">
-                                <a href='#' title={inactive ? 'Home' : ''}>
+                            <Link className = "nav-items-link" to='/home' title={inactive ? 'Home' : ''}>
+                                <li style={!inactive ? { width: "210px" } : { width: "20px" }} className="home-icon">
                                     <div className='menu-icon imported-i'>
                                         <i class="bi bi-house"></i>
                                     </div>
                                     <span className={!inactive ? 'menu-name' : 'hidden'}>Home</span>
-                                </a>
-                            </li>
-                            <li style={!inactive ? {width : "210px"} : {width : "20px"}}>
-                                <a href='#' title={inactive ? 'About' : ''}>
+                                </li>
+                            </Link>
+                            <Link className = "nav-items-link" to='/home' title={inactive ? 'Home' : ''}>
+                                <li style={!inactive ? { width: "210px" } : { width: "20px" }}>
                                     <div className='menu-icon imported-i'>
                                         <i class="bi bi-info-square"></i>
                                     </div>
                                     <span className={!inactive ? 'menu-name' : 'hidden'}>About</span>
-                                </a>
-                            </li>
-                            <li style={!inactive ? {width : "210px"} : {width : "20px"}}>
-                                <a href='#' title={inactive ? 'Events' : ''}>
+                                </li>
+                            </Link>
+                            <Link className = "nav-items-link" to='/home' title={inactive ? 'Home' : ''}>
+                                <li style={!inactive ? { width: "210px" } : { width: "20px" }}>
                                     <div className='menu-icon'>
                                         <img height={14.5} src={EventsLogo} />
                                     </div>
                                     <span className={!inactive ? 'menu-name' : 'hidden'}>Events</span>
-                                </a>
-                            </li>
-                            <li style={!inactive ? {width : "210px"} : {width : "20px"}}>
-                                <a href='#' title={inactive ? 'Sponsors' : ''}>
+                                </li>
+                            </Link>
+                            <Link className = "nav-items-link" to='/home' title={inactive ? 'Home' : ''}>
+                                <li style={!inactive ? { width: "210px" } : { width: "20px" }}>
                                     <div className='menu-icon imported-i'>
                                         <i class="bi bi-info-square"></i>
                                     </div>
                                     <span className={!inactive ? 'menu-name' : 'hidden'}>Sponsors</span>
-                                </a>
-                            </li>
-                            <li style={!inactive ? {width : "210px"} : {width : "20px"}}>
-                                <Link to="/core" title={inactive ? 'Core' : ''}>
+                                </li>
+                            </Link>
+                            <Link className = "nav-items-link" to='/core' title={inactive ? 'Core' : ''}>
+                                <li style={!inactive ? { width: "210px" } : { width: "20px" }}>
                                     <div className='menu-icon imported-i'>
                                         <i class="bi bi-info-square"></i>
                                     </div>
                                     <span className={!inactive ? 'menu-name' : 'hidden'}>Core</span>
-                                </Link>
-                            </li>
+                                </li>
+                            </Link>
                         </ul>
                     </div>
 
