df9113017fd0a7b5ce1440752913c177b6ad22b6
Tanisha <tanishagupta0820@gmail.com>
2022-10-15 18:59:29 +0530

    final nav

--- src/Sidenav.js
+++ src/Sidenav.js
@@ -19,69 +19,98 @@ function Sidenav() {
 
                 <div className='faded-hz-line'></div>
 
-                <div className='main-menu-container'>
-                    <div className='menu-items'>
-                        <ul className='menu-list'>
-                            <Link className="nav-items-link" onClick={toggleNav} to='/' title={inactive ? 'Home' : ''}>
-                                <li style={!inactive ? { width: "210px" } : { width: "20px" }} className="home-icon">
-                                    <div className='menu-icon imported-i'>
-                                        <i class="bi bi-house"></i>
-                                    </div>
-                                    <span className={!inactive ? 'menu-name' : 'hidden'}>Home</span>
-                                </li>
-                            </Link>
+                    <div className='main-menu-container'>
+                        <div className='menu-items'>
+                            <ul className='menu-list'>
+                                <Link className="nav-items-link" onClick={toggleNav} to='/' title={inactive ? 'Home' : ''}>
+                                    <li style={!inactive ? { width: "210px" } : { width: "20px" }} className="home-icon">
+                                        <div className='menu-icon'>
+                                            <i class="bi bi-house"></i>
+                                        </div>
+                                        <span className={!inactive ? 'menu-name' : 'hidden'}>Home</span>
+                                    </li>
+                                </Link>
 
+                                <div className='faded-hz-line'></div>
+
+                                <Link className="nav-items-link" onClick={toggleNav} to='/home' title={inactive ? 'Home' : ''}>
+                                    <li style={!inactive ? { width: "210px" } : { width: "20px" }}>
+                                        <div className='menu-icon'>
+                                            <i class="bi bi-info-square"></i>
+                                        </div>
+                                        <span className={!inactive ? 'menu-name' : 'hidden'}>About</span>
+                                    </li>
+                                </Link>
+
+                                <div className='faded-hz-line'></div>
+
+                                <Link className="nav-items-link" onClick={toggleNav} to='/home' title={inactive ? 'Home' : ''}>
+                                    <li style={!inactive ? { width: "210px" } : { width: "20px" }}>
+                                        <div className='menu-icon'>
+                                            <i class="bi bi-flag"></i>
+                                        </div>
+                                        <span className={!inactive ? 'menu-name' : 'hidden'}>Events</span>
+                                    </li>
+                                </Link>
+
+                                <div className='faded-hz-line'></div>
+
+                                <Link className="nav-items-link" onClick={toggleNav} to='/home' title={inactive ? 'Home' : ''}>
+                                    <li style={!inactive ? { width: "210px" } : { width: "20px" }}>
+                                        <div className='menu-icon'>
+                                            <i class="bi bi-currency-rupee"></i>
+                                        </div>
+                                        <span className={!inactive ? 'menu-name' : 'hidden'}>Sponsors</span>
+                                    </li>
+                                </Link>
+
+                                <div className='faded-hz-line'></div>
+
+                                <Link className="nav-items-link" onClick={toggleNav} to='/core' title={inactive ? 'Core' : ''}>
+                                    <li style={!inactive ? { width: "210px" } : { width: "20px" }}>
+                                        <div className='menu-icon'>
+                                            <i class="bi bi-people"></i>
+                                        </div>
+                                        <span className={!inactive ? 'menu-name' : 'hidden'}>Team</span>
+                                    </li>
+                                </Link>
+
+                                <div className='faded-hz-line'></div>
+
+                            </ul>
+
+                            <ul className='menu-list'>
+                                
                             <div className='faded-hz-line'></div>
 
-                            <Link className="nav-items-link" onClick={toggleNav} to='/home' title={inactive ? 'Home' : ''}>
-                                <li style={!inactive ? { width: "210px" } : { width: "20px" }}>
-                                    <div className='menu-icon imported-i'>
-                                        <i class="bi bi-info-square"></i>
-                                    </div>
-                                    <span className={!inactive ? 'menu-name' : 'hidden'}>About</span>
-                                </li>
-                            </Link>
+                                <Link className="nav-items-link" onClick={toggleNav} to='/'>
+                                    <li style={!inactive ? { width: "210px" } : { width: "20px" }} className="insta-icon">
+                                        <div className='insta-icon'>
+                                            <i class="bi bi-instagram"></i>
+                                        </div>
+                                        <span className={!inactive ? 'menu-name' : 'hidden'}>@ignite</span>
+                                    </li>
+                                </Link>
 
-                            <div className='faded-hz-line'></div>
+                                <div className='faded-hz-line'></div>
 
-                            <Link className="nav-items-link" onClick={toggleNav} to='/home' title={inactive ? 'Home' : ''}>
-                                <li style={!inactive ? { width: "210px" } : { width: "20px" }}>
-                                    <div className='menu-icon'>
-                                        <i class="bi bi-flag"></i>
-                                    </div>
-                                    <span className={!inactive ? 'menu-name' : 'hidden'}>Events</span>
-                                </li>
-                            </Link>
+                                <Link className="nav-items-link" onClick={toggleNav} to='/'>
+                                    <li style={!inactive ? { width: "210px" } : { width: "20px" }} className="linkedin-icon">
+                                        <div className='linkedin-icon'>
+                                        <i class="bi bi-linkedin"></i>
+                                        </div>
+                                        <span className={!inactive ? 'menu-name' : 'hidden'}>/ignite</span>
+                                    </li>
+                                </Link>
 
-                            <div className='faded-hz-line'></div>
+                                <div className='faded-hz-line'></div>
 
-                            <Link className="nav-items-link" onClick={toggleNav} to='/home' title={inactive ? 'Home' : ''}>
-                                <li style={!inactive ? { width: "210px" } : { width: "20px" }}>
-                                    <div className='menu-icon'>
-                                        <i class="bi bi-currency-rupee"></i>
-                                    </div>
-                                    <span className={!inactive ? 'menu-name' : 'hidden'}>Sponsors</span>
-                                </li>
-                            </Link>
+                            </ul>
+                        </div>
 
-                            <div className='faded-hz-line'></div>
-
-                            <Link className="nav-items-link" onClick={toggleNav} to='/core' title={inactive ? 'Core' : ''}>
-                                <li style={!inactive ? { width: "210px" } : { width: "20px" }}>
-                                    <div className='menu-icon'>
-                                        <i class="bi bi-people"></i>
-                                    </div>
-                                    <span className={!inactive ? 'menu-name' : 'hidden'}>Team</span>
-                                </li>
-                            </Link>
-
-                            <div className='faded-hz-line'></div>
-
-                        </ul>
                     </div>
                 </div>
             </div>
-        </div>
     )
 }
 
