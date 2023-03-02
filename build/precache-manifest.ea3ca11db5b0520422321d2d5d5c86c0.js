f6c134187085f632554e20d470e89c4f94197789
Tanisha <tanishagupta0820@gmail.com>
2023-03-02 10:28:31 +0530

    build contents

---
+++ build/precache-manifest.ea3ca11db5b0520422321d2d5d5c86c0.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c369e771485659205d7d815ebd9a320b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "df8c0863e8c73e6bf636",
+    "url": "/static/css/main.fa869bef.chunk.css"
+  },
+  {
+    "revision": "0d58c1905dc75174289b",
+    "url": "/static/js/2.b8d31aed.chunk.js"
+  },
+  {
+    "revision": "78d8353da635afbf52949a3d44b951a3",
+    "url": "/static/js/2.b8d31aed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "385405cab3c1e2d289d8",
+    "url": "/static/js/3.7f8cfa27.chunk.js"
+  },
+  {
+    "revision": "df8c0863e8c73e6bf636",
+    "url": "/static/js/main.4093edee.chunk.js"
+  },
+  {
+    "revision": "111ef872ccf0c323b406",
+    "url": "/static/js/runtime-main.62876fdd.js"
+  },
+  {
+    "revision": "280b42aeacb0dc2bf39b8de7ca9bf7c1",
+    "url": "/static/media/cp.280b42ae.png"
+  },
+  {
+    "revision": "1e670d88b23c7ab956f1829e3828a210",
+    "url": "/static/media/digital-7.1e670d88.ttf"
+  },
+  {
+    "revision": "33e292e9b0973d16d3917f9c556f2f19",
+    "url": "/static/media/esports.33e292e9.jpg"
+  },
+  {
+    "revision": "9cd0d6ba41431b858d225ad214623d79",
+    "url": "/static/media/exclusive-paper.9cd0d6ba.png"
+  },
+  {
+    "revision": "fd2bf68f0e480da224e9f629038e016b",
+    "url": "/static/media/hackathon.fd2bf68f.png"
+  },
+  {
+    "revision": "2b66d6657bae3e8ecb35a1784cb7577d",
+    "url": "/static/media/layered-peaks.2b66d665.svg"
+  },
+  {
+    "revision": "7426287ed1d4aef01d34ab3351a0caae",
+    "url": "/static/media/marky.7426287e.jpg"
+  },
+  {
+    "revision": "76758553f059fe2847996acb83747125",
+    "url": "/static/media/shortFilmEditing.76758553.png"
+  },
+  {
+    "revision": "fc030d9b45e1e24511b982942f0d233c",
+    "url": "/static/media/star.fc030d9b.png"
+  },
+  {
+    "revision": "7603bbb792942623db500a65b3230697",
+    "url": "/static/media/techTalk.7603bbb7.png"
+  },
+  {
+    "revision": "428285cc941237a23c357ae4dc7b0f95",
+    "url": "/static/media/upscaled_ignite.428285cc.png"
+  }
+]);
